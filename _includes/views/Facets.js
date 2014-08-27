6d715ed40573cba25b4d79b2f89941bfac3a8c67
jueyang <jueyang22@gmail.com>
2014-08-27 17:03:19 -0400

    set Facets.js as an example for scope binding

--- _includes/views/Facets.js
+++ _includes/views/Facets.js
@@ -2,39 +2,44 @@ views.Facets = Backbone.View.extend({
 	el: '#filter-items',
 	template:_.template('<div id="<%= id %>" class="topics"></div>'),
 	initialize:function(){
+		_.bind(this,'render');
 		this.collection = new Facets();
 		this.render();
 	},
 	render:function(){
-		var that = this;
 		var facetHTML = '';
 
 		// a global object to host one view for each facet
 		// in their respective element
+		// this can be cleaned up
 		global.allFacetViews = {}
 
 		this.collection.each(function(facet){
-			facetHTML += that.template(facet);
+			facetHTML += this.template(facet);
 
 			facet.subFilters.fetch({
 				success: function (data) {
-				global.allFacetViews[facet.id] = new views.Filters({
+				global.allFacetViews[facet.id] = new views.Filters({ // views.Filters is being reused
 					el: '#' + facet.id, // element name is created based on the facet name
 					collection: facet.subFilters
 				});
 
-				_.each(global.processedFacets, function (obj) {
-					if (obj.collection === facet.id) {
-						global.allFacetViews[facet.id].active = true
-					}
-				});
+				// search in processedFacets if current facet is selected
+				if (global.processedFacets.indexOf(facet.id) >=0) {
+					// if so, the facet is "active"
+					// the active value is being used in Filters etc
+					// WHY using the view as true 
+					global.allFacetViews[facet.id].active = true
+				}
+
 				facet.subFilters.watch();
 
-				// that.counter++;
-				// if (that.counter === facets.length) updateDescription();
 				}
 			});
-		})
+
+		},this) // ensure the context refers to the view, so that var that = this is not needed
+
+		console.log(global.allFacetViews)
 
 		this.$el.html(facetHTML); // create the topics/facets divs
 	}
